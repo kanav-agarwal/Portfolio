3bc2dc899c8a83d9be2223e3db54bb7d857cad85
kanav-23 <agarwal.kanav.2008@gmail.com>
2024-12-17 17:19:56 -0700

    Third commit

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-function typeWriter(elementClass, text, speed){
+function typeWriter(elementId, cursorId, text, speed){
     let i = 0
-    let element = document.getElementsByClassName(elementClass)
-
-    let interval = setInterval(addLetter, speed)
+    let element = document.getElementById(elementId)
+    let cursor = document.getElementById(cursorId)
 
     function addLetter(){
         if(i < text.length){
-            element[0].innerHTML += text.charAt(i)
+            element.innerHTML += text.charAt(i)
             i++
         }
         else{
-            clearInterval(interval)
+            clearInterval(textInterval)
+            clearInterval(cursorInterval)
+            cursor.style.visibility = 'hidden'
         } 
     }  
+
+    function blinkingCursor(){
+        cursor.style.visibility = cursor.style.visibility === "hidden" ? "visible":"hidden"; 
+    }
+
+    let cursorInterval = setInterval(blinkingCursor, 250)
+    let textInterval = setInterval(addLetter, speed)
 }
+
+
 let timePerChar = 175
-typeWriter("typing1", "Kanav Agarwal", timePerChar)
-setTimeout(function(){typeWriter("typing2", "Programmer and Web Developer", timePerChar)}, 13 * timePerChar)
-setTimeout(function(){typeWriter("typing3", "View my work", timePerChar)}, (13 + 28) * timePerChar)
+setTimeOut(function(){typeWriter("typing1", "cursor1", "Kanav Agarwal", timePerChar)}, 0)
+setTimeout(function(){typeWriter("typing2", "cursor2", "Programmer and Web Developer", timePerChar)}, 13 * timePerChar)
+setTimeout(function(){typeWriter("typing3", "cursor3", "View my work", timePerChar)}, (13 + 28) * timePerChar)
